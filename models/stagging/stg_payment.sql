8ec75b8f257cb2a7e3a5e316cc41076475da2680
yuri.morales.h@gmail.com <yuri.morales.h@gmail.com>
2022-09-12 00:33:57 +0000

    dd

--- models/stagging/stg_payment.sql
+++ models/stagging/stg_payment.sql
@@ -9,7 +9,7 @@ with payment as (
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-    from `dbt-tutorial.stripe.payment`
-
+    --from `dbt-tutorial.stripe.payment`
+    from {{ source('stripe', 'payment') }}
 )
 select * from payment
